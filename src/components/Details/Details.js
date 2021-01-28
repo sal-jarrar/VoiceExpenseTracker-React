93db63306602400fb67285ecb0cae23f8f15b23a
sultan <sultanjarrar89@gmail.com>
2021-01-27 23:53:09 -0800

    adding speechly

--- src/components/Details/Details.js
+++ src/components/Details/Details.js
@@ -3,17 +3,21 @@ import { Card, CardHeader, CardContent, Typography } from '@material-ui/core'
 import { Doughnut } from 'react-chartjs-2'
 
 import useStyles from './styles'
-function Details({ title }) {
+import useTransactions from '../../useTransactions'
+
+const DetailsCard = ({ title, subheader }) => {
+  const { total, chartData } = useTransactions(title)
   const classes = useStyles()
+
   return (
     <Card className={title === 'Income' ? classes.income : classes.expense}>
-      <CardHeader title={title} />
+      <CardHeader title={title} subheader={subheader} />
       <CardContent>
-        <Typography variant='h5'>$500</Typography>
-        {/* <Doughnut data={chartData} /> */}
+        <Typography variant='h5'>${total}</Typography>
+        <Doughnut data={chartData} />
       </CardContent>
     </Card>
   )
 }
 
-export default Details
+export default DetailsCard
--- src/index.js
+++ src/index.js
@@ -1,13 +1,15 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import App from './App'
-import {Provider} from './Context/context'
+import { Provider } from './Context/context'
+import { SpeechProvider } from '@speechly/react-client'
 import './index.css'
 
 ReactDOM.render(
-  <Provider>
-    <App />
-  </Provider>,
-
+  <SpeechProvider appId='7c4aee08-1073-4a32-b862-ebe1850e0732' language='en-US'>
+    <Provider>
+      <App />
+    </Provider>
+  </SpeechProvider>,
   document.getElementById('root')
 )
