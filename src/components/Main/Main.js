9e4bcf96a4d2acaa040c837cf6b3b6302672843b
sal-jarrar <eng.sultanj@gmail.com>
2022-09-10 15:15:36 -0700

    fix bug

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -1,4 +1,4 @@
-import React, { useContext } from 'react'
+import React, { useContext } from "react";
 import {
   Card,
   CardHeader,
@@ -6,28 +6,28 @@ import {
   Typography,
   Grid,
   Divider,
-} from '@material-ui/core'
+} from "@material-ui/core";
 
-import { ExpenseTrackerContext } from '../../context/context'
-import useStyles from './styles'
-import Form from './Form/Form'
-import List from './List/List'
-import InfoCard from '../InfoCard'
+import { ExpenseTrackerContext } from "../../context/context";
+import useStyles from "./styles";
+import Form from "./Form/Form";
+import List from "./List/List";
+import InfoCard from "../InfoCard";
 
 const ExpenseTracker = () => {
-  const classes = useStyles()
-  const { balance } = useContext(ExpenseTrackerContext)
+  const classes = useStyles();
+  const { balance } = useContext(ExpenseTrackerContext);
 
   return (
     <Card className={classes.root}>
-      <CardHeader title='Expense Tracker' subheader='Powered by Speechly' />
+      <CardHeader title="Expense Tracker" subheader="Powered by Speechly" />
       <CardContent>
-        <Typography align='center' variant='h5'>
+        <Typography align="center" variant="h5">
           Total Balance ${balance}
         </Typography>
         <Typography
-          variant='subtitle1'
-          style={{ lineHeight: '1.5em', marginTop: '20px' }}
+          variant="subtitle1"
+          style={{ lineHeight: "1.5em", marginTop: "20px" }}
         >
           <InfoCard />
         </Typography>
@@ -42,7 +42,7 @@ const ExpenseTracker = () => {
         </Grid>
       </CardContent>
     </Card>
-  )
-}
+  );
+};
 
-export default ExpenseTracker
+export default ExpenseTracker;
--- src/index.js
+++ src/index.js
@@ -1,15 +1,15 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-import App from './App'
-import { Provider } from './context/context'
-import { SpeechProvider } from '@speechly/react-client'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
+import { Provider } from "./context/context";
+import { SpeechProvider } from "@speechly/react-client";
+import "./index.css";
 
 ReactDOM.render(
-  <SpeechProvider appId='690e083a-c773-4bad-a3ad-0a6723b163d3' language='en-US'>
+  <SpeechProvider appId="690e083a-c773-4bad-a3ad-0a6723b163d3" language="en-US">
     <Provider>
       <App />
     </Provider>
   </SpeechProvider>,
-  document.getElementById('root')
-)
+  document.getElementById("root")
+);
